55b5fb8a9e9832bf8bc7c9e197d4fd0509e308aa
tingwei0509 <b0222017@ems.niu.edu.tw>
2017-08-30 15:54:15 +0800

    RR?
    RRRRR

--- slave/main.c
+++ slave/main.c
@@ -1,10 +1,13 @@
 
+
+//RRRRRRRRRRR
+
 /*
-#include "../ASALIB/ASA_DEVICE.h"
-#include "STP00.h"
+ #include "../ASALIB/ASA_DEVICE.h"
+ #include "STP00.h"
 
 
-int main() {
+   int main() {
     char phase[4] = {15-(1<<PHA),15-(1<<PHB),15-(1<<PHC),15-(1<<PHD)};
     uint8_t i=0;
     PH_DDR = 15;
@@ -17,8 +20,8 @@ int main() {
 
 
     return 0;
-}
-*/
+   }
+ */
 //void uart_init(/* arguments */) {}
 
 //--------------------------------------------------------
@@ -42,63 +45,63 @@ char Data_Buffer[3]={0};
 //--------------SPI_INI
 void spi_ini(void) {
 
-printf("SPI_INI\n");
-      DDRB|=(0<<MOSI)|(1<<MISO)|(0<<SCK)|(0<<SS);   //set MISO
-      PORTB|=(1<<MISO);                             //MISO Output ,others input
-      SPCR=(1<<SPIE)|(1<<SPE)|(0<<MSTR);			//Enable
-printf("End\n");
+        printf("SPI_INI\n");
+        DDRB|=(0<<MOSI)|(1<<MISO)|(0<<SCK)|(0<<SS); //set MISO
+        PORTB|=(1<<MISO);                           //MISO Output ,others input
+        SPCR=(1<<SPIE)|(1<<SPE)|(0<<MSTR);    //Enable
+        printf("End\n");
 
 }
 //--------------SPI_DATA_SWAP
 char data_swap(char Tdata) {
 
-	 char timeout=0;
-	 char Rdata=0;
-	 static char count=0;
-	 SPDR=Tdata;
-	 while(!(SPSR&(1<<SPIF))){}
-	 Rdata=SPDR;
-	 count=count%3;
-	 Data_Buffer[count]=Rdata;
-	 count++;
-	 return 0;
+        char timeout=0;
+        char Rdata=0;
+        static char count=0;
+        SPDR=Tdata;
+        while(!(SPSR&(1<<SPIF))) {}
+        Rdata=SPDR;
+        count=count%3;
+        Data_Buffer[count]=Rdata;
+        count++;
+        return 0;
 
 }
 //-------------timer
 void timer_set(void) {
 
-	TCCR0A = (1<<WGM01);
-  TCCR0B = (1<<CS00);
-	TIMSK0=1<<OCIE0A;
-	OCR0A=255;
+        TCCR0A = (1<<WGM01);
+        TCCR0B = (1<<CS00);
+        TIMSK0=1<<OCIE0A;
+        OCR0A=255;
 
 
 }
 //-------------main
 
 int main (void) {
-	spi_ini();
-	timer_set;
-	sei();
-  char i=0;
+        spi_ini();
+        timer_set;
+        sei();
+        char i=0;
 
-	while(1) {
+        while(1) {
 
-		switch (Data_Buffer[i]) {
+                switch (Data_Buffer[i]) {
 
-			case 1:
-            break;
-      case 2:
-            break;
-      case 3:
-            break;
-      default: break;
-
-
-		}
+                case 1:
+                        break;
+                case 2:
+                        break;
+                case 3:
+                        break;
+                default: break;
 
 
-	}
+                }
+
+
+        }
 
 
 
@@ -112,6 +115,6 @@ int main (void) {
 
 ISR(TIMER0_COMP) {
 
-	data_swap(1);
+        data_swap(1);
 
 }
