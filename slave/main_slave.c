71f5b434edf95ed35fcfd6c83a056b270f904b20
XRAY <jared12222@gmail.com>
2018-03-06 12:13:01 +0800

    Edit <Unfinished>

--- slave/main_slave.c
+++ slave/main_slave.c
@@ -2,32 +2,46 @@
 #include <util\delay.h>
 #include <avr\interrupt.h>
 #include <stdio.h>
+#include <inttypes.h>
 
 #include ".\LIB\rev_STDIO.h"
 #include ".\LIB\uart.h"
-#include ".\LIB\fifo.h"
 
-typedef struct stepper_module_setting_format
+typedef struct serial_packet_format
 {
-    uint8_t RW;
-    uint8_t index;
-    uint8_t data;
-    uint8_t result;
-}stp_set;
+    volatile uint8_t status;
+    volatile uint8_t Header_count;
+    volatile uint8_t RW;
+    volatile uint8_t index;
+    volatile uint8_t data;
+    volatile uint8_t result;
+} serial_packet;
 
-fifo_set rx_fifo;
+#define serial_packet_initial {0,0,0,0,0,0}
 
-// 封包解讀
-uint8_t package_read(fifo_set *ptr)
-{
-    
-    return 0;
-}
+serial_packet rec_state = serial_packet_initial;
 
+// Packet receiver status
+#define Pst_Header 1
+#define Pst_UID    2
+#define Pst_ADDR   3
+#define Pst_BYTES  4
+#define Pst_DATA   5
+#define Pst_Chksum 6
+
+#define P_Header 0xAA
 
 ISR(USART_RX_vect)
 {
-    fifo_push(&rx_fifo,1,&(UDR));
+    switch (rec_state.status) {
+        case Pst_Header:{
+            if(UDR == P_Header)
+                rec_state.Header_count++;
+            break;
+        }
+        case
+
+    }
 }
 
 int main(void)
@@ -39,7 +53,6 @@ int main(void)
 
     while(1)
     {
-        chk = fifo_pop(&rx_fifo,1,&data);
         if(!chk)
         {
             //printf("%u\n",data);
