963e25b685cb29b1cf8b76ecffff0e6097077d4b
XRAY <jared12222@gmail.com>
2018-03-14 07:17:43 +0800

    Edit: Test Serial slave

--- slave/main_slave.c
+++ slave/main_slave.c
@@ -8,39 +8,34 @@
 #include ".\LIB\uart.h"
 #include ".\LIB\serial_packet.h"
 #include ".\LIB\serial_packet_print.h"
+#include ".\LIB\fifo.h"
 //#include "..\rev_lib\ASA\ASA_UARTS.h"
 
+fifo_set usart_rx_fifo = fifo_initial;
 serial_packet rec_state = serial_packet_initial;
+
+ISR(USART1_RX_vect){
+    fifo_push(&usart_rx_fifo,1,&UDR);
+}
 
 int main(void)
 {
     rev_STDIO_set();
     USART_set();
+    sei();
     uint8_t chk,data;
-    //sei();
     printf("Start\n");
-    // DDRB |= 7 << 5;
-    // DDRF |= 1 << 4;
-    // PORTB = 1 << 5;
-    // PORTF = 1 << 4;
     while(1)
     {
-    //    USART_receive(&data);
-        printf("In: ");
-        scanf("%u",&data);
-        chk = unpack(&rec_state,&data);
-        serial_packet_stat(&rec_state);
-        serial_packet_chk(&chk);
-        // chk = uarts_decode_buf_step();
-        // if(chk != BUF_NULL)
-        // {
-        //     printf("UID:   %u\n", UartDecoder.uid);
-        //     printf("WR :   %u\n", UartDecoder.wr);
-        //     printf("ADDR:  %u\n", UartDecoder.address);
-        //     printf("BYTES: %u\n", UartDecoder.bytes);
-        //     printf("DATA:  %u\n", UartDecoder.data);
-        //     printf("CHKSUM:%u\n", UartDecoder.chksum);
-        // }
-
+        // printf("In: ");
+        // scanf("%u",&data);
+        chk = fifo_pop(&usart_rx_fifo,1, &data);
+        if(chk){
+            chk = unpack(&rec_state,&data); // unpack by continuous sending a byte data
+            serial_packet_stat(&rec_state); // print out struct
+            serial_packet_chk(&chk);    // print out ERROR
+        }
+        else
+            serial_packet_timeout_count(&rec_state);
     }
 }
