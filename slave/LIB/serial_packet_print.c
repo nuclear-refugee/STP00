796453f9900f734170ae558e606ab3b9b9a09f55
XRAY <jared12222@gmail.com>
2018-03-13 12:09:13 +0800

    Add: print out serial packet

---
+++ slave/LIB/serial_packet_print.c
@@ -0,0 +1,48 @@
+#include "serial_packet_print.h"
+
+void serial_packet_chk(uint8_t *chk){
+    switch(*chk)
+    {
+        case ERROR_HEADER:
+            printf("ERROR HEADER\n");
+            break;
+        case ERROR_UID:
+            printf("ERROR UID\n");
+            break;
+        case ERROR_CHKSUM:
+            printf("ERROR_CHKSUM\n");
+            break;
+        case Packet_OK:
+            printf("Packet OK\n");
+            break;
+    }
+}
+
+void serial_packet_stat(serial_packet *sp)
+{
+    printf("--- Information ---\n");
+    printf("Status:");
+    switch(sp->status){
+        case Pst_HEADER:
+            printf("HEADER\n");
+            break;
+        case Pst_UID:
+            printf("UID\n");
+            break;
+        case Pst_ADDR:
+            printf("ADDR\n");
+            break;
+        case Pst_DATA:
+            printf("DATA\n");
+            break;
+        case Pst_CHKSUM:
+            printf("CHKSUM\n");
+            break;
+    }
+    // printf("Status : %u\n",sp->status);
+    printf("WR     : %u\n",sp->WR);
+    printf("ADDR   : %u\n",sp->addr);
+    printf("Data   : %u\n",sp->data);
+    printf("Chksum : %u\n",sp->chksum);
+    printf("--- END ---\n");
+}
---
+++ slave/LIB/serial_packet_print.h
@@ -0,0 +1,11 @@
+#ifndef __SERIAL_PACKET_PRINT_H__
+#define __SERIAL_PACKET_PRINT_H__
+
+#include "serial_packet.h"
+#include <stdio.h>
+#include <inttypes.h>
+
+void serial_packet_chk(uint8_t *chk);
+void serial_packet_stat(serial_packet *sp);
+
+#endif
